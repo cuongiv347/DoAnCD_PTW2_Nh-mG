106ae4413b89024f46a9cab82aba0c9a35923cd5
Tun Dz <33826364+hotungho92@users.noreply.github.com>
2019-10-24 11:34:53 +0700

    module5

---
+++ Tun_PTWeb2/js/app.js
@@ -0,0 +1,55 @@
+
+var trus = new Vue({
+    el: '#trus',
+    data: {
+        message: 'TRUSTED BY',
+        message2: 'These fine folks trusted tha award winning restaurant'
+
+
+    }
+})
+var ins = new Vue({
+    el: '#ins',
+    data: {
+        message3: 'INSTAGRAM',
+        message4: 'We love posting photos of our coustomers having a good time'
+
+    }
+}) 
+
+var sub = new Vue({
+    el: '#sub',
+    data: {
+        message5: 'SUBSCRIBE',
+        message6: 'Get updates about new dishes and upcoming events',
+
+
+    }
+}) 
+var foot = new Vue({
+    el: '#foot',
+    data: {
+        message7: 'ABOUT US',
+        message8: 'Duis leo justo, condimentum at purus eu,Aenean sed dolor sem. Etiam massa libero, auctor vitae egestas et, accumsan quis nunc.Duis leo justo, condimentum at purus eu, posuere pretium tellus.',
+        message9:'Read more →',
+        message10:'RECENT POST',
+        message11:'Hand picked ingredients for our best customers',
+        message12:'29 may 2015',
+        message13:'Daily special foods that you will going to love',
+        message14:'29 may 2015',
+        message15:'REACH US',
+        message16:'28 Seventh Avenue, Neew York, 10014',
+        message17:'Phone: (415) 124-5678',
+        message18:'[support@restaurant.com]',
+
+    }
+}) 
+
+new Vue({
+    el: '#app',
+    components: {
+      VueSlider: window['vue-slider-component']
+    }
+
+  })
+
