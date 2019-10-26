abff8efd3a17bb963f1042fc2dd66e93573e179b
hoangtuanthai17 <44045204+hoangtuanthai17@users.noreply.github.com>
2019-10-26 19:47:49 +0700

    upload module 7

---
+++ module_7/menu_all.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Tomato Responsive Restaurant HTML5 Template</title>
+    <meta name="author" content="Surjith S M">
+
+    <meta name="description" content="Tomato is a Responsive HTML5 Template for Restaurants and food related services.">
+    <meta name="keywords" content="tomato, responsive, html5, restaurant, template, food, reservation">
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="css/plugin.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="body">
+        <div class="main-wrapper">
+            <section class="menu menu2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="page-header wow fadeInDown">
+                                <h1 id="title_menu">Menu</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="food-menu wow fadeInUp">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="menu-tags4">
+                                    <span data-filter="*" class="tagsort4-active">All</span>
+                                    <span data-filter=".starter">starters</span>
+                                    <span data-filter=".breakfast">breakfast</span>
+                                    <span data-filter=".lunch">lunch</span>
+                                    <span data-filter=".dinner">dinner</span>
+                                    <span data-filter=".desserts">desserts</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row menu-items4">
+                            <div class="menu-item4 col-sm-4 col-xs-12 starter dinner desserts">
+                                <div class="menu-info">
+                                    <img src="img/menu/1/1.jpg" class="img-responsive" alt="" />
+                                    <a href="./menu_all.html">
+                                        <div class="menu4-overlay">
+                                            <h4>English-Asparagus</h4>
+                                            <p>Asparagus, hens egg, toasted
+                                                <br>sunflower seeds, Spenwood cheese</p>
+                                            <span class="price">$ 14.95</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="menu-item4 col-sm-4 col-xs-12 starter">
+                                <div class="menu-info">
+                                    <a href="./menu_all.html">
+                                        <img src="img/menu/1/2.jpg" class="img-responsive" alt="" />
+                                        <div class="menu4-overlay">
+                                            <h4>English-Asparagus</h4>
+                                            <p>Asparagus, hens egg, toasted
+                                                <br>sunflower seeds, Spenwood cheese</p>
+                                            <span class="price">$ 14.95</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="menu-item4 col-sm-4 col-xs-12 breakfast desserts starter">
+                                <div class="menu-info">
+                                    <a href="./menu_all.html">
+                                        <img src="img/menu/1/3.jpg" class="img-responsive" alt="" />
+                                        <div class="menu4-overlay">
+                                            <h4>English-Asparagus</h4>
+                                            <p>Asparagus, hens egg, toasted
+                                                <br>sunflower seeds, Spenwood cheese</p>
+                                            <span class="price">$ 14.95</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="menu-item4 col-sm-4 col-xs-12 breakfast">
+                                <div class="menu-info">
+                                    <a href="./menu_all.html">
+                                        <img src="img/menu/1/4.jpg" class="img-responsive" alt="" />
+                                        <div class="menu4-overlay">
+                                            <h4>English-Asparagus</h4>
+                                            <p>Asparagus, hens egg, toasted
+                                                <br>sunflower seeds, Spenwood cheese</p>
+                                            <span class="price">$ 14.95</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="menu-item4 col-sm-4 col-xs-12 lunch starter breakfast">
+                                <div class="menu-info">
+                                    <a href="./menu_all.html">
+                                        <img src="img/menu/1/5.jpg" class="img-responsive" alt="" />
+                                        <div class="menu4-overlay">
+                                            <h4>English-Asparagus</h4>
+                                            <p>Asparagus, hens egg, toasted
+                                                <br>sunflower seeds, Spenwood cheese</p>
+                                            <span class="price">$ 14.95</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="menu-item4 col-sm-4 col-xs-12 lunch">
+                                <div class="menu-info">
+                                    <a href="./menu_all.html">
+                                        <img src="img/menu/1/6.jpg" class="img-responsive" alt="" />
+                                        <div class="menu4-overlay">
+                                            <h4>English-Asparagus</h4>
+                                            <p>Asparagus, hens egg, toasted
+                                                <br>sunflower seeds, Spenwood cheese</p>
+                                            <span class="price">$ 14.95</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <script src="js/vendor/jquery-1.11.2.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/vendor/isotope.pkgd.min.js"></script>
+    <script src="js/vendor/slick.min.js"></script>
+    <script src="js/vendor/vegas/vegas.min.js"></script>
+    <script src="js/vendor/jquery.mb.YTPlayer.js"></script>
+    <script src="js/vendor/jquery.stellar.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
