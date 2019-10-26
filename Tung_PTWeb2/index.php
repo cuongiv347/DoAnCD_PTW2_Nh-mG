beaa343b5a294106a6636915fa6ee784edba63ae
Tun Dz <33826364+hotungho92@users.noreply.github.com>
2019-10-26 08:16:50 +0700

    DO an

---
+++ Tung_PTWeb2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/swiper.min.css">
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="flexslider.css">
+</head>
+<body>
+    
+
+<?php include '../WEB/module8.php'; ?>
+<?php include '../WEB/module9.php'; ?>
+
+<?php include '../WEB/module11.php'; ?>
+
+<script src="js/vue.js"></script> 
+<script src="js/app.js"></script>
+
+</body>
+</html>
---
+++ Tung_PTWeb2/module8.php
@@ -0,0 +1,83 @@
+
+<section class="trusted" id="trus">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header wow fadeInDown">
+                        <h1> {{message}}<small>{{message2}}</small></h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row wow fadeInUp">
+                <div class="col-md-12">
+                    <div class="trusted-sponsors">
+                        <img src="img/sponsor/foodsquare.png" alt="sponsors">
+                        <img src="img/sponsor/opentable.png" alt="sponsors">
+                        <img src="img/sponsor/tripadvisor.png" alt="sponsors">
+                        <img src="img/sponsor/zomato.png" alt="sponsors">
+                        <img src="img/sponsor/foodsquare.png" alt="sponsors">
+                        <img src="img/sponsor/opentable.png" alt="sponsors">
+                        <img src="img/sponsor/tripadvisor.png" alt="sponsors">
+                        <img src="img/sponsor/zomato.png" alt="sponsors">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="trusted-quote">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-offset-1 col-md-10">
+                        <div class="  vue-slider-component">
+                            <div class="swiper-wrapper">
+                          
+                          
+                                        <div class="swiper-slide">
+                                            <li>
+                                                <img src="img/quote.png" alt="quote">
+                                                <p class="quote-body">The world’s a big, big stage for this notorious
+                                                    deli smack
+                                                    in
+                                                    the middle of the theatre district, infamously famous for its
+                                                    over-the-top
+                                                    corned beef and pastrami sandwiches, chopped liver, blintzes,
+                                                    celebrities,
+                                                    salami, smoked fish and New York’s finest cheesecake.</p>
+                                                <p class="quote-author">Anthony Reed, <span>New York</span></p>
+                                            </li>
+                                            <li>
+                                                <img src="img/quote.png" alt="quote">
+                                                <p class="quote-body">You might not find dragon meat on the menu, but
+                                                    you’ll
+                                                    find
+                                                    pretty much anything else that walks, swims or flies, cooked up in
+                                                    more ways
+                                                    than there are people in the Guangdong province. This Midtown
+                                                    mainstay has a
+                                                    20-year history of delivering mouth-watering and Cantonese style
+                                                    chow.</p>
+                                                <p class="quote-author">Gemma Arterton, <span>Bay Area</span></p>
+                                            </li>
+                                            <li>
+                                                <img src="img/quote.png" alt="quote">
+                                                <p class="quote-body">This NYC historical landmark in the heart of the
+                                                    Theatre
+                                                    District has been serving up suds and down home pub food since 1892,
+                                                    surviving
+                                                    prohibition by renting the front of its then Rockefeller Center
+                                                    façade to
+                                                    Greek
+                                                    florists, while the Hurley brothers ran a speak-easy in back.</p>
+                                                <p class="quote-author">Zachary Burton, <span>Sanfransisco</span></p>
+                                            </li>
+                               
+                                      
+                             
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+ 
+   
