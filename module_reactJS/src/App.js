ac2ce778688625d5947d499386546617febcf03b
hoangtuanthai17 <44045204+hoangtuanthai17@users.noreply.github.com>
2019-10-28 21:57:51 +0700

    module reactjs

---
+++ module_reactJS/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Module from './Module/Module';
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+      <h1>React JS</h1>
+      <Module/>
+      </div>
+    );
+  }
+}
+
+export default App;
