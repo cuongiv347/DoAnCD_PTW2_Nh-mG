8d405660fd4b83f8d538f9272cd2ffee54f5a099
PHAMCHELINH <phamchilinh15112016@gmail.com>
2019-10-30 13:09:42 +0700

    Vuejs
    module1,module2

--- Linh_PTWeb2/js/appVue2.js
+++
@@ -1,9 +0,0 @@
-var appVue1 = new Vue({
-    el: '#appVue1',
-    data: {
-        img1: 'images/logo.png',
-        Delicious: 'DELICIOUS Food',
-        Tomato: 'Tomato is a delitious restaurant website template',
-        img2: 'images/arrow-down.png'
-    }
-});
